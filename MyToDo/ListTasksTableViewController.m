34fe3c6b10bd874a789ed91b03d1d7eebae21347
Adauto <stanleystl@gmail.com>
2012-05-03 22:10:29 -0300

    Delete task

--- MyToDo/ListTasksTableViewController.m
+++ MyToDo/ListTasksTableViewController.m
@@ -108,19 +108,13 @@
 }
 */
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+        [Task deleteTask:[self.fetchedResultsController objectAtIndexPath:indexPath]];
+    }  
 }
-*/
 
 /*
 // Override to support rearranging the table view.
